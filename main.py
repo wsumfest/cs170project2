01180dfa8cef83a8960b5b804b8ff7cecce9719e
wsumfest <was022@berkeley.edu>
2016-05-01 21:51:42 -0700

    updated framework with instance_number and finished skeleton

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import os
-import sys
-
+import operator
 
 class Problem():
     """A representation of the graph problem that we are working with"""
-    def __init__(self, num_nodes=0, child_nodes=[], adj_matrix=[]):
+    def __init__(self, instance_number, num_nodes=0, child_nodes=[], adj_matrix=[]):
+        self.instance_number = instance_number
         self.num_nodes = num_nodes
         self.child_nodes = child_nodes
         self.adj_matrix = adj_matrix
         self.child_set = set(child_nodes)
+        self.vertices = []
+        self.edges = {}
+        self.solution = ""
 
     def set_num_nodes(self, num):
         self.num_nodes = num
@@ -27,7 +30,20 @@ class Problem():
         self.adj_matrix = mat
 
     def get_adj_matrix(self):
-        return self.get_adj_matrix
+        return self.adj_matrix
+
+    def set_edges_and_vertices(self):
+        for num in range(0,self.num_nodes - 1):
+            self.vertices.append(num)
+            (self.edges)[num] = []
+            for j in range(0,self.num_nodes - 1):
+                if (self.get_adj_matrix())[num][j] == 1:
+                    (self.edges)[num].append(j)
+
+    def get_solution(self):
+        return self.solution
+
+
 
 
 "strips the string of the new line character, returns an integer"
@@ -53,7 +69,23 @@ def get_array_from_string(string):
             continue
     return output
 
+
+"""Takes an instance of our problem and writes to our output file,
+   transforms into vertices and edges to make search easier """
+def compute(problem, output_file):
+    problem.set_edges_and_vertices()
+
+
+    
+
+
+
+
+
 def main(directory):
+
+    problems = []
+
     output_file = "solutions.out"
 
     for root, folder, files in os.walk(directory):
@@ -62,8 +94,12 @@ def main(directory):
         for fil in files:
             if fil == ".DS_Store":
                 continue
+
+            instance_stripped = fil[:-3]
+            file_number = int(instance_stripped)
             filepath = os.path.join(root, fil)
-            problem_instance = Problem()
+            problem_instance = Problem(instance_number=file_number)
+            problems.append(problem_instance)
             handle = open(filepath, "r")
             ### sets the number of nodes in the problem instance ###
             num_nodes = handle.readline()
@@ -78,13 +114,21 @@ def main(directory):
             while new_line:
                 vector = get_array_from_string(new_line)
                 problem_instance.adj_matrix.append(vector)
-
                 new_line = handle.readline()
-            
-            
+            ### close the handle because we are done reading data from input ###
             handle.close()
+            ### Now we can start working on our problem ###
+            compute(problem_instance, write_handle)
+
+        ### Sort problems by instance number, we are ready to write to output ###
+        problems.sort(key=operator.attrgetter("instance_number"))
+        for problem in problems:
+            write_handle.write(problem.get_solution())
+
         write_handle.close()
 
 if __name__ == "__main__":
     input_dir = "phase1-processed"
     main(input_dir)
+
+
